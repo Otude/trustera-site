be4b8ec9946afdcd23e7f3aeda10269a5367b582
Otude <148293439+Otude@users.noreply.github.com>
2026-05-03 13:51:09 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -246,7 +246,7 @@ export default function TrusteraLandingPage() {
           <iframe
             src="https://tally.so/r/XxOEvO"
             width="100%"
-            height="700"
+            height="900"
             frameBorder="0"
             marginHeight="0"
             marginWidth="0"
